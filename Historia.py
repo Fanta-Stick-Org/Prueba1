6a2b6a6370a04de224f90720b01ab7bc2925af0e
Coraline <90431692+bluecoraline@users.noreply.github.com>
2021-09-11 10:46:57 -0500

    Update Historia.py

--- Historia.py
+++ Historia.py
@@ -5,3 +5,6 @@ print("Érase en un lugar cuyo nombre no me quiero acordar, vivía en Noruega un
         "que saltaban pescados con alas que se comían unos a otros; los observaba mientras el gélido viento del " + 
         "invierno azotaba su pelaje, ya que sabía que necesitaba cazar toda la comida que pudiera obtener antes que " +
         "el ragnarok  comenzará, porque después de eso no quedaría nada sobre la tierra… ni siquiera el.")
+
+print("El dia empezo como cualquier otro, el oso se lavanto, sacudio su pelaje y fue camino al rio, de repente se dio cuenta que no habian peces, 
+      miro a su alreder y habian tiendas de acampar por doquier, humo y mucho ruido...")
