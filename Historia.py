41d3a6666ced984306fb5dd16a258386dd5d7965
Rhaegar09 <71042961+Rhaegar09@users.noreply.github.com>
2021-09-11 10:59:56 -0500

    Create Historia.py

--- Historia.py
+++ Historia.py
@@ -6,8 +6,12 @@ print("Érase en un lugar cuyo nombre no me quiero acordar, vivía en Noruega un
         "invierno azotaba su pelaje, ya que sabía que necesitaba cazar toda la comida que pudiera obtener antes que " +
         "el ragnarok  comenzará, porque después de eso no quedaría nada sobre la tierra… ni siquiera el.")
 
-print("El dia empezo como cualquier otro, el oso se lavanto, sacudio su pelaje y fue camino al rio, de repente se dio cuenta que no habian peces," +
+print("El dia empezo como cualquier otro, el oso se lavanto, sacudio su pelaje y fue camino al rio, de repente se dio cuenta que no habian peces," 
       "miro a su alredor y habian tiendas de acampar por doquier, humo y mucho ruido...")
 
 print("Rápidamente entro en un estado de pánico por el ruido molesto que venia de las tiendas de campaña"+
       "se estaba preparando para realizar un ataque contra los invasores...")
+
+print("Cuando estaba por atacar, se dio cuenta que habia un bebé que estaba gateando hacia una cascada, instintivamente corrió a salvarlo y lo llevó a su madre"+
+     "Cuando su madre vio que el oso salvó a su hijo, para agradecerle le dió una canasta llena de peces, por lo cual el oso la tomó y se tranquilizó, ya que se dio cuenta"+
+     "que a pesar de ser invasores, tenian familia y solo querian conseguir alimentos.")
